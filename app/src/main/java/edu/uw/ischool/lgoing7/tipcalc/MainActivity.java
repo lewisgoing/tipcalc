b7d8fa70a4f67a9f7b90c74750e520c48b387a30
lewisgoing <lgoing7@uw.edu>
2024-10-23 18:27:34 -0700

    Completed assignment + Extra Credit

---
+++ app/src/main/java/edu/uw/ischool/lgoing7/tipcalc/MainActivity.java
@@ -0,0 +1,6 @@
+package edu.uw.ischool.lgoing7.tipcalc;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/edu/uw/ischool/lgoing7/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/lgoing7/tipcalc/MainActivity.kt
@@ -1,20 +1,50 @@
 package edu.uw.ischool.lgoing7.tipcalc
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextAmount: EditText
+    private lateinit var buttonCalculateTip: Button
+    private lateinit var spinnerTipPercentage: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        editTextAmount = findViewById(R.id.editTextAmount)
+        buttonCalculateTip = findViewById(R.id.buttonCalculateTip)
+        spinnerTipPercentage = findViewById(R.id.spinnerTipPercentage)
+
+        // setup dropdown for tip percentages
+        val percentages = arrayOf("10%", "15%", "18%", "20%")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, percentages)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTipPercentage.adapter = adapter
+
+        // format input and enable button
+        editTextAmount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val amount = s.toString().toDoubleOrNull()
+                buttonCalculateTip.isEnabled = amount != null
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        // calc tip on button click
+        buttonCalculateTip.setOnClickListener {
+            val amount = editTextAmount.text.toString().toDoubleOrNull() ?: return@setOnClickListener
+            val selectedPercentage = spinnerTipPercentage.selectedItem.toString().removeSuffix("%").toDouble()
+            val tip = amount * (selectedPercentage / 100)
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            Toast.makeText(this, "Tip Amount: $formattedTip", Toast.LENGTH_LONG).show()
         }
     }
 }
